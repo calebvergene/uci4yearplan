099b2640f22ec2af4eae31cb3a9081e44e3bc1fd
caleb vergene <calebverg@gmail.com>
2025-02-06 23:17:02 -0800

    redeploy check

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -2,10 +2,6 @@ import React from 'react'
 
 const Sidebar = async () => {
 
-  // Fetch majors
-  const res = await fetch('https://anteaterapi.com/v2/rest/programs/majors?limit=1');
-  const majors = await res.json();
-  
 
   return (
     <div>Sidebar</div>
